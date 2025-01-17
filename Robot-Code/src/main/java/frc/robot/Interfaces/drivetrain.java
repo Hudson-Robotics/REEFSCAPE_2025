f422020ba061704a8ff0cbd4eb3ea8c744bda900
HudsonRobotics4295 <hudsonisdrobotics@gmail.com>
2025-01-17 16:44:03 -0600

    1/17/25

--- Robot-Code/src/main/java/frc/robot/Interfaces/drivetrain.java
+++ Robot-Code/src/main/java/frc/robot/Interfaces/drivetrain.java
@@ -3,3 +3,4 @@ package frc.robot.Interfaces;
 public interface drivetrain {
     void drive(double xspeed, double yspeed, double rotation);
 }
+
--- Robot-Code/src/main/java/frc/robot/RobotContainer.java
+++ Robot-Code/src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.swervedrive;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -23,6 +24,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final SecurityCam camera;
+ private final swervedrive drivetrain = new swervedrive();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
