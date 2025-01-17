0034252689ffc4be3d2287cb3d139dae3ffed997
HudsonRobotics4295 <hudsonisdrobotics@gmail.com>
2025-01-17 16:44:03 -0600

    1/17/25

--- Robot-Code/src/main/java/frc/robot/RobotContainer.java
+++ Robot-Code/src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.swervedrive;
+import frc.robot.subsystems.swervedrive;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -25,7 +26,6 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final SecurityCam camera;
- private final swervedrive drivetrain = new swervedrive();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
