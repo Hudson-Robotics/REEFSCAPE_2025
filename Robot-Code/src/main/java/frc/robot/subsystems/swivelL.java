b6705f3b578ac9c61742979d5a88d0424824882d
tommy129 <josephvazquez2172@gmail.com>
2025-02-22 10:11:58 -0600

    new code 2/22/25

--- Robot-Code/src/main/java/frc/robot/subsystems/swivelL.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/swivelL.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Interfaces.swivel;
 import frc.robot.Constants.SparkMaxIDs;
 import javax.sound.sampled.Port;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.XboxController;
@@ -12,28 +14,33 @@ import edu.wpi.first.wpilibj.XboxController;
 public class swivelL extends SubsystemBase implements swivel{
     final SparkMax swivelMotor;
     private XboxController controller; 
-public void swivel(XboxController)
-public void Swivel(1);
-    controller = new XboxController(port1);
+public swivelL(XboxController controller) {
+    this.controller = controller;
+    this.swivelMotor = new SparkMax(SparkMaxIDs.SWIVEL_MOTOR_UP, MotorType.kBrushed);
+}
 
-    SwivelMotor = SparkMax(SparkMaxIDs.SWIVEL_MOTOR_UP, MotorType.kBrushed);
+@Override
+public void periodic() {
+    double speedX = this.controller.getLeftX();
+    swivelMotor.set(.05);
+    swivelMotor.set(-.05);
 }
-    @Override
-    public void periodic() {
-     double speedX = this.controller.getLeftX();
+
+@Override
+public void up() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'up'");
 }
-    @Override
-    public void up() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'up'");
-    }
-    @Override
-    public void down() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'down'");
-    }
-    @Override
-    public void stop() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'stop'");
-    }
+
+@Override
+public void down() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'down'");
+}
+
+@Override
+public void stop() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'stop'");
+}
+}
