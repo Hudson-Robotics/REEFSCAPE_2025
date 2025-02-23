aafc9e0d8cafd3be2869d0e282f1bc6ceb0c8786
tommy129 <josephvazquez2172@gmail.com>
2025-02-22 23:24:54 -0600

    LED.CODE

---
+++ Robot-Code/src/main/java/frc/robot/subsystems/Led.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+
+public class Led extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  Spark ledLights = new Spark(2);
+  public Led() {}
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    ledLights.set(.9);
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+
+  public void setLedToRedAlliance() {
+    ledLights.set(.9);
+  }
+
+  public void setLedToBlueAlliance() {
+    ledLights.set(.5);
+  }
+}
