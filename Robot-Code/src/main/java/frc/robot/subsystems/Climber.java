df43b21c5725b82745b41955dd41fecab9260b62
JuanBarron2001 <barronjuan2001@gmail.com>
2025-02-24 23:10:20 -0600

    Use Motor in Climber

--- Robot-Code/src/main/java/frc/robot/subsystems/Climber.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/Climber.java
@@ -5,36 +5,33 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Interfaces.Climb;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import frc.robot.Constants.SparkMaxIDs;
 
+import frc.robot.Interfaces.Motors.Motor;
 
 public class Climber extends SubsystemBase implements Climb  {
- final SparkMax upMotor1;
- final SparkMax UpMotor2;
+ final Motor upMotor1;
+ final Motor UpMotor2;
 
  private XboxController controller; 
- public Climber() { 
+ public Climber(Motor upMotor1, Motor upMotor2) { 
     controller = new XboxController(2);
-    upMotor1 = new SparkMax(SparkMaxIDs.UP_MOTOR_CLIMB_ONE, MotorType.kBrushed);
-    UpMotor2 = new SparkMax(SparkMaxIDs.UP_MOTOR_CLIMB_TWO, MotorType.kBrushed);
+    this.upMotor1 = upMotor1;
+    this.UpMotor2 = upMotor2;
 }
 
     @Override
      public void periodic() {
-      // Get the state of the A button (boolean)
-     boolean xButtonPressed = controller.getAButton();
-      SmartDashboard.putBoolean("X Button Pressed", xButtonPressed); 
-      SmartDashboard.putNumber("upMotor1", 1); 
-      upMotor1.set(1); 
-      SmartDashboard.putNumber("upMotor2", 1); 
-      UpMotor2.set(1);   
+        // Get the state of the A button (boolean)
+        boolean xButtonPressed = controller.getAButton();
+        SmartDashboard.putBoolean("X Button Pressed", xButtonPressed); 
+        if(xButtonPressed){
+            this.climb(1);
+        }
      }
 
     @Override
     public void climb(double speed) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'climb'");
+        this.upMotor1.setSpeed(speed);
+        this.UpMotor2.setSpeed(speed);
     }
 }
