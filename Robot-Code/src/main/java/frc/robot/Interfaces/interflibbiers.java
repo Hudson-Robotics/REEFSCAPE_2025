eb12042c35292189f30615032d98b3edf20d8b9d
HudsonRobotics4295 <hudsonisdrobotics@gmail.com>
2025-02-25 18:07:26 -0600

    new

---
+++ Robot-Code/src/main/java/frc/robot/Interfaces/interflibbiers.java
@@ -0,0 +1,8 @@
+package frc.robot.Interfaces;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+public interface interflibbiers
+
+public class flibbirs extends SubsystemBase implements interflibbiers {
+
+}
