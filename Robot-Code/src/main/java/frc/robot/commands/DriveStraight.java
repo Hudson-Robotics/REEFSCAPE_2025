2ab1c8901f3aa60bffdf1e910f4443f48b69295b
JuanBarron2001 <barronjuan2001@gmail.com>
2025-03-01 23:08:53 -0600

    Adding Straight Shot Auto back

--- Robot-Code/src/main/java/frc/robot/commands/DriveStraight.java
+++ Robot-Code/src/main/java/frc/robot/commands/DriveStraight.java
@@ -25,7 +25,7 @@ public class DriveStraight extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    swerveDrive.drive(.1, .1, 0);
+    swerveDrive.drive(0, .1, 0);
   }
 
   // Called once the command ends or is interrupted.
