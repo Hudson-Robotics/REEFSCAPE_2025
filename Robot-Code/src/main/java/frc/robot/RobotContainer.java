410f93cd7e275a04479d1dbf00dad05e9dd94ea3
HeliosMoros <127363205+HeliosMoros@users.noreply.github.com>
2025-03-06 13:44:23 -0600

    robot bindings

--- Robot-Code/src/main/java/frc/robot/RobotContainer.java
+++ Robot-Code/src/main/java/frc/robot/RobotContainer.java
@@ -1,9 +1,60 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+//
+// Copyright 2021-2025 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 
 package frc.robot;
 
+// import com.pathplanner.lib.*;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import frc.robot.generated.TunerConstants;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj.PS4Controller.Button; //change to Dualsense or add dual sense
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.Constants.WristConstants;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.OIConstants;
+import frc.robot.commands.DriveCommands;
+import frc.robot.commands.MainCommands;
+import frc.robot.commands.AMoveEnd;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.drive.GyroIO;
+import frc.robot.subsystems.drive.ModuleIO;
+import frc.robot.subsystems.drive.ModuleIOSim;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+import java.util.List;
+// look at vector8177_2025robot & team9181's 2025 robot
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.Constants.SparkMaxIDs;
 import frc.robot.Interfaces.Motors.MotorWithEncoder;
@@ -17,6 +68,7 @@ import frc.robot.subsystems.Led;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -26,20 +78,54 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
- private final SwerveDrive drivetrain = this.createSwerveDrive();
-  //private final SecurityCam camera;
- private final SwerveDrive drivetrain = new SwerveDrive();
- private final Led leds = new Led();
+  private final SwerveDrive drivetrain = this.createSwerveDrive();
+  private final Wrist wrist
+  private final Drive drive;
+  private final Intake intake;
+  private final Climber climber;
+  private final Elevator elevator;
+  public final DriveSubsystem m_robotDrive = new DriveSubsystem();
+  private SendableChooser<Command> autoChooser = new SendableChooser<Command>();
 
+  //private final SecurityCam camera;
+  private final SwerveDrive drivetrain = new SwerveDrive();
+  private final Led leds = new Led();
+ 
+  // Dashboard inputs
+  private final LoggedDashboardChooser<Command> autoChooser;
+
+  // The driver's and operators controller
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
+  private final CommandXboxController m_driverController = 
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  //private final CommandXboxController driverController = new CommandXboxController(0);
+  //private final CommandXboxController operatorController = new CommandXboxController(1);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+    // Configure the button bindings
+    configureButtonBindings();
     //camera = new SecurityCam();
+    autoChooser.setDefaultOption("Cross Auto Line Only", new AMoveEnd(m_robotDrive));
+    autoChooser.addOption("Do Nothing",
+      new RunCommand(
+        ()-> m_robotDrive.drive(0.0,0.0,0.0,true), m_robotDrive)
+    );
+
+    SmartDashboard.putData("Auto Choices", autoChooser);
+    // Configure default commands
+    m_robotDrive.setDefaultCommand(
+        // The left stick controls translation of the robot.
+        // Turning is controlled by the X axis of the right stick.
+        new RunCommand(
+            () -> m_robotDrive.drive(
+                -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
+                true),
+            m_robotDrive));
   }
 
   /**
