911b2128046c05776c00d4affbb744ea342d85cb
chjones56 <chj101@gmail.com>
2025-03-13 15:17:02 -0500

    Move

--- Robot-Code/src/main/java/frc/robot/commands/Autos.java
+++ Robot-Code/src/main/java/frc/robot/commands/Autos.java
@@ -18,3 +18,35 @@ public final class Autos {
     throw new UnsupportedOperationException("This is a utility class!");
   }
 }
+
+private final DriveSubsystem _drive;
+private double startTime;
+private final long tolerance = 12;
+
+public AMoveEnd(DriveSubsystem drive) {
+    _drive = drive;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drive);
+}
+// Called when the command is initially scheduled.
+@Override
+public void initialize(){
+    startTime = Timer.getFPGATimestamp();
+}
+
+// Called every time the scheduler runs while the command is scheduled.
+@Override
+public void execute(){
+    //_drive.drive(0.5, 0.0, 0.0, true);
+}
+// Called once the command ends or is interrupted.
+@Override
+public void end(boolean interrupted) {
+    //_drive.setX();
+}
+
+// Returns true when the command should end.
+@Override
+public boolean isFinished() {
+    return (Timer.getFPGATimestamp()-startTime>=0.9);
+}
